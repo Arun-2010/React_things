390c9a03c9e9466ba8b84bca199bb3b9650bf5ca
Arun-2010 <arunbandari15@gmail.com>
2025-09-19 23:46:54 +0530

    c

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 function Api() {
+  const [data, setData] = useState([]);
+
   const products = () => {
     const api = "https://fakestoreapi.com/products";
     axios.get(api).then(products => {
       console.log(products);
+      setData(products.data);
     });
   }
 
@@ -17,10 +20,25 @@ function Api() {
   }
 
   return (
+    <>
     <div className='p-4 flex gap-4 justify-center '>
         <button onClick={products} className='bg-blue-500 text-white p-2 rounded-md'>products</button>
         <button onClick={addproduct} className='bg-green-500 text-white p-2 rounded-md '>addproduct</button>
     </div>
+
+    <hr />
+    <ul>
+      {data.length > 0 ? (
+          data.map((p) => (
+              <li className="rounded-md bg-pink-500 flex gap-2 p-2">{p.title}</li>
+          ))
+      ) : (
+          <li>No products found</li>
+      )}
+     </ul>
+
+    </>
+    
   )
 }
 
